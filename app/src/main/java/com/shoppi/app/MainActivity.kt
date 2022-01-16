5ae2dcb641423c26404222aab645d982923af7d4
KimJeong-hyeon <bark0525@naver.com>
2022-01-16 17:02:46 +0900

    Splash 화면 구성 (31미만 버전과 31이상 버전을 구분)

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -2,10 +2,14 @@ package com.shoppi.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+        Log.d(TAG,"onCreate")
     }
 }
