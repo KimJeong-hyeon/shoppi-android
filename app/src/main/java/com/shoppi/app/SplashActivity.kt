3c71c5befcfbfdbc15149e87466851a3ca33c8d0
KimJeong-hyeon <bark0525@naver.com>
2022-01-16 19:22:54 +0900

    Splash화면 오류 수정 및 Bottom Navigation UI 구현

--- app/src/main/java/com/shoppi/app/SplashActivity.kt
+++ app/src/main/java/com/shoppi/app/SplashActivity.kt
@@ -2,13 +2,12 @@ package com.shoppi.app
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity: AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         startActivity(Intent(this, MainActivity::class.java))
         finish()
     }
